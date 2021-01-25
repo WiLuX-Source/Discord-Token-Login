59b9a4417186fa7c0a923dc33ec640932ce81db0
WiLuX <61833703+WiLuX-Source@users.noreply.github.com>
2021-01-26 02:03:40 +0300

    Version 1.2

--- Token Login Latest.user.js
+++ Token Login Latest.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         DC Token Login
-// @version      1.1
+// @version      1.2
 // @description  Allows You To Login Your Account Without Inputting Your E-mail And Password.
 // @author       WiLuX & Wonfy
 // @match        *://discord.com/login
@@ -16,31 +16,30 @@ function login(token) {
 }
 
 window.addEventListener('load', () => {
-    const form = document.getElementsByClassName("authBoxExpanded-2jqaBe authBox-hW6HRx theme-dark")[0];
+    const target = document.querySelectorAll("div")[7]
     const accounts = {
-        'Token1': "Account #1",
-        'Token2': "Account #2",
-        'Token3': "Account #3",
-        'Token4': "Account #4",
-        'Token5': "Account #5",
-        'Token6': "Account #6",
-        'Token7': "Account #7",
-        'Token8': "Account #8",
-        'Token9': "Account #9",
-        'Token10': "Account #10"
+        'Token1': "1",
+        'Token2': "2",
+        'Token3': "3",
+        'Token4': "4",
+        'Token5': "5",
+        'Token6': "6",
+        'Token7': "7",
+        'Token8': "8",
+        'Token9': "9",
+        'Token10': "10"
     };
     const showButton = document.createElement("button");
     var container = document.createElement("div");
     var hidden = false;
-    container.style = "display : none";
-
+    container.style = "display : none;";
     showButton.innerText = "Show Accounts";
     showButton.className = "showybutton"
     var style = document.createElement("style");
-    let css = ".showybutton{background-color: #7289DA; width:167.812px; border: none; color: #FFF; padding: 16px 32px; text-align: center; display: inline-block; font-size: 16px; line-height: 24px; margin-bottom: 0px; margin-top: 20px; border-radius: 100px; transition: background-color .17s ease,color .17s ease;}";
+    let css = ".showybutton{position:absolute;top:180px;background-color: #7289DA; width:167.812px; border: none; color: #FFF; padding: 16px 32px; text-align: center; display: inline-block; font-size: 16px; line-height: 24px; margin-bottom: 0px; margin-top: 20px; border-radius: 10px; transition: background-color .17s ease,color .17s ease;}";
     css += ".showybutton:hover{background-color: #677bc4;}";
     css += ".showybutton:active{background-color: #5b6eae;}";
-    css += ".accounts{background-color: #7289DA; height: 44px; width: 112px; border: none; color: #FFF; padding: 0px; text-align: center; display: inline-block; font-size: 16px; line-height: 24px; margin-top: 20px; border-radius: 5px; margin-right: 20px; transition: background-color .17s ease,color .17s ease;}";
+    css += ".accounts{background-color: #7289DA; height: 50px; width: 50px; border: none; color: #FFF; padding: 0px; text-align: center; display: inline-block; font-size: 16px; line-height: 24px; transition: background-color .17s ease,color .17s ease;}";
     css += ".accounts:hover{background-color: #677bc4;}";
     css += ".accounts:active{background-color: #5b6eae;}";
 
@@ -53,11 +52,11 @@ window.addEventListener('load', () => {
 
     showButton.addEventListener("click", () => {
         showButton.innerText = hidden ? "Show Accounts" : "Hide Accounts";
-        container.style = hidden ? "display : none" : "display : block";
+        container.style = hidden ? "display : none; width : min-content" : "display : block; width : min-content;";
         hidden = !hidden;
     })
 
-    form.append(showButton);
+    target.append(showButton);
 
     for (const [token, name] of Object.entries(accounts)) {
         var button = document.createElement("button");
@@ -68,6 +67,6 @@ window.addEventListener('load', () => {
         });
         container.append(button);
     }
-    form.append(container);
+    target.append(container);
 
 }, false);
