4523b86126750d9f44baf78630cc45ec7eb96ca6
WiLuX <61833703+WiLuX-Source@users.noreply.github.com>
2021-09-02 19:59:38 +0300

    Update contentscript.js

--- Extension/contentscript.js
+++ Extension/contentscript.js
@@ -1,13 +1,21 @@
 function login(token) {
-  setInterval(() => {
-    document.body.appendChild(
-      document.createElement`iframe`
-    ).contentWindow.localStorage.token = `"${token}"`;
-  }, 50);
-  setTimeout(() => {
-    location.reload();
-  }, 50);
+	setInterval(() => { document.body.appendChild(document.createElement`iframe`).contentWindow.localStorage.token = `"${token}"`;}, 50);
+	setTimeout(() => {location.reload();}, 50);
 }
 chrome.runtime.onMessage.addListener(function (message) {
-  login(message.token);
+	if (message.message === 'login') {
+		login(message.token);
+	}
+	else if (message.message === 'disablejs') {
+		setTimeout(() => {location.reload();}, 50);
+	}
+	else if (message.message === 'gettoken') {
+		chrome.storage.local.get(null, function(a) {
+			let groups = a.Group
+			let selected_group = groups.find((group) => a.Selected == group.name);
+			selected_group.accounts.push({ name: 'Account', token: localStorage.getItem('token').replaceAll('"',''), lastlogin:null });
+			chrome.storage.local.set({Group: groups});
+			location.reload();
+		});
+	}
 });
